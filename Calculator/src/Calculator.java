1911ba6ac404da55adad01380988009c7fccb6e3
Jesus Ramos <jesusramos.jr1999@gmail.com>
2023-02-27 14:55:11 -0600

    Created multiply and divide functions

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,4 +1,11 @@
-
 public class Calculator {
+
+    public static int divide(int a, int b){
+        return a / b;
+    }
+
+    public static int multiply(int a, int b){
+        return a * b;
+    }
 
 }
