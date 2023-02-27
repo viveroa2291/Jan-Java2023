850d7cdfe0312dfecb018b89505d122cd11f0991
viveroa2291 <viveroa2291@uwec.edu>
2023-02-27 14:52:30 -0600

    Addition Method
    Adding the addition method.

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,4 +1,10 @@
 
 public class Calculator {
-
+	public static main void (String [] args) {
+		
+		System.out.println(addition(2, 4));
+		public static int addition(int a, int b) {
+			return a + b;
+		}
+	}
 }
